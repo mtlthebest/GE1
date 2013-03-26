ff44afa74453fb0009574694f7c68c9cc2e1c798
mtlthebest <mtl@libero.it>
2013-03-27 00:19:07 +0100

    Minor change
    UTF8 -> UTF-8

--- common-open-page.inc.php
+++ common-open-page.inc.php
@@ -2,7 +2,7 @@
 include("common-set-connection.inc.php");  // variabili di connessione al database MySQL
 include("common-set-xml.inc.php");         // usato per impostare le pagine XHTML (-> XML)
 include("common-set-doctype.inc.php");     // usato per impostare il DOCTYPE (XHTML 1.1)
-include("common-set-header.inc.php");      // usa $title per impostare il titolo della pagina web; imposta la codifica UTF8, il foglio di stile, ecc...
+include("common-set-header.inc.php");      // usa $title per impostare il titolo della pagina web; imposta la codifica UTF-8, il foglio di stile, ecc...
 include("common-display-heading.inc.php"); // stampa l'intestazione della pagina web in modo uniforme
 include("common-functions.inc.php");       // rende disponibili le principali funzioni utilizzate
 ?>
